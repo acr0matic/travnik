6bef8ebcf3277d00a51ddcb51d271683d1fdc3e0
acr0matic <work.acr0matic@gmail.com>
2022-09-22 18:19:39 +0300

    feat: мобильное меню

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,11 +1,39 @@
 const header = document.getElementById('header');
 
 if (header) {
+  const hero = document.querySelector('.hero');
+  const heroHeight = hero.offsetHeight;
+  let isReach = false;
+
   MaxHeight(header);
   window.addEventListener('resize', () => MaxHeight(header));
 
   function MaxHeight(target) {
     const height = target.offsetHeight;
     document.documentElement.style.setProperty('--header-height', height + 'px');
+  }
+
+  window.addEventListener('scroll', () => {
+    isReach = window.scrollY >= heroHeight;
+
+    if (isReach) header.classList.add('header-background');
+    else header.classList.remove('header-background');
+  });
+
+  const mobile = header.querySelector('#mobile-menu');
+  const mobileNav = mobile.querySelectorAll('.nav__link');
+  const burger = header.querySelector('.header__action');
+
+  burger.addEventListener('click', () => Menu());
+
+  mobileNav.forEach(link => {
+    link.addEventListener('click', () => Menu());
+  });
+
+  function Menu() {
+    if (!isReach) header.classList.toggle('header-background');
+
+    mobile.classList.toggle('mobile-menu--open');
+    burger.firstElementChild.classList.toggle('is-active')
   }
 }
