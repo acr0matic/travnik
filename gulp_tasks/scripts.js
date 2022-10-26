3dd0fff6aa57c56fb91a5c19f1652df36874dc36
acr0matic <work.acr0matic@gmail.com>
2022-10-26 19:38:21 +0300

    fix: убран Babel

--- gulp_tasks/scripts.js
+++ gulp_tasks/scripts.js
@@ -15,7 +15,7 @@ const paths = require('../gulpfile');
 */
 
 task('scripts', () => src(paths.scripts.src)
-  .pipe(babel())
+  // .pipe(babel())
   .pipe(addsrc.prepend(paths.scripts.polyfills))
   .pipe(addsrc.prepend(paths.scripts.libraries))
   .pipe(concat(paths.scripts.out))
