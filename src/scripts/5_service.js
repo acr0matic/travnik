a05e7d0bf19f1a0b59a526fc76a2021a8e1a9ffa
acr0matic <work.acr0matic@gmail.com>
2022-10-27 17:00:11 +0300

    feat: переработаны услуги

--- src/scripts/5_service.js
+++ src/scripts/5_service.js
@@ -9,6 +9,11 @@ if (service) {
     const index = button.dataset.index;
 
     button.addEventListener('click', (e) => {
+      const active = service.querySelector('.card--active');
+      if (active) active.classList.remove('card--active');
+
+      card.classList.add('card--active');
+
       SLIDER_SERVICE_CONTENT.slideTo(index);
       collapse.classList.remove('content-collapse--hidden');
     });
